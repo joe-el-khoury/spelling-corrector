7bcef1f7753375f755c85daa80dded1ca851206a
Joe Khoury <joe.el.k97@gmail.com>
2016-06-08 17:12:08 -0400

    Converted spaces to tabs just to see what it looks like.

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -6,16 +6,16 @@
  * Opens the file stream.
  */
 FileReader::FileReader (const std::string& _file_name) {
-	(this->fs).open(_file_name);
-	if ((this->fs).fail()) {
-		std::cout << "The file <" << _file_name << "> could not be opened." << std::endl;
-		(this->fail) = true;
-	}
+    (this->fs).open(_file_name);
+    if ((this->fs).fail()) {
+        std::cout << "The file <" << _file_name << "> could not be opened." << std::endl;
+        (this->fail) = true;
+    }
 }
 
 /**
  * When the object is destroyed, the stream has to be closed.
  */
 FileReader::~FileReader () {
-	(this->fs).close();
+    (this->fs).close();
 }
