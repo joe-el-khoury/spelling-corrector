6493d40a6ec05cc6eb39725231754c2a7fe08938
Joe Khoury <joe.el.k97@gmail.com>
2016-06-13 01:26:38 -0400

    Implemented the new constructor of the Tokenizer object. Nothing else is done, and this DOES NOT COMPILE.

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -1,32 +1,33 @@
+#include <iostream>
 #include <memory>
 #include <algorithm>
 
 #include "Tokenizer.h"
+#include "FileReader.h"
 #include "Token.h"
 
-Tokenizer::Tokenizer (const std::string& _str_to_tokenize) {
-	this->tokens = Tokenizer::TokenList();
-	this->str_to_tokenize = _str_to_tokenize;
-	this->tokenize(' ');
+Tokenizer::Tokenizer (const std::string& _file_name) : fr(_file_name) {
 }
 
 void Tokenizer::tokenize (char _delimiter) {
-	size_t start = 0;
-	size_t end   = (this->str_to_tokenize).find_first_of(_delimiter);
+    size_t start = 0;
+    size_t end   = (this->str_to_tokenize).find_first_of(_delimiter);
 
-	while (end <= std::string::npos) {
-		std::string token_substr = (this->str_to_tokenize).substr(start, end-start);
-		if (token_substr.length() > 0) {
-			Token token_to_add(token_substr);
-			token_to_add.cleanup_token();
-			(this->tokens).push_back(token_to_add);
-		}
+    while (end <= std::string::npos) {
+        // Get the string from the start position to the delimiter.
+        std::string token_substr = (this->str_to_tokenize).substr(start, end-start);
+        if (token_substr.length() > 0) {
+            Token token_to_add(token_substr);
+            // Remove any ugly characters from the token.
+            token_to_add.cleanup_token();
+            (this->tokens).push_back(token_to_add);
+        }
 
-		if (end == std::string::npos) {
-			break;
-		}
+        if (end == std::string::npos) {
+            break;
+        }
 
-		start = end+1;
-		end = str_to_tokenize.find_first_of(_delimiter, start);
-	}
+        start = end+1;
+        end = str_to_tokenize.find_first_of(_delimiter, start);
+    }
 }
--- src/Tokenizer.h
+++ src/Tokenizer.h
@@ -1,24 +1,23 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
-#include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
 #include <memory>
 
 #include "Token.h"
+#include "FileReader.h"
 
 class Tokenizer {
-	// A type that is just a list of Tokens.
-	typedef std::vector<Token> TokenList;
+    // A type that is just a list of Tokens.
+    typedef std::vector<Token> TokenList;
 public:
-	Tokenizer (const std::string&);
+    Tokenizer (const std::string&);
     void tokenize (char);
 private:
     Tokenizer::TokenList tokens;
     FileReader fr;
-	std::string str_to_tokenize;
+    std::string str_to_tokenize;
 };
 
 #endif /* TOKENIZER_H */
