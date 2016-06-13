1e13c50b7c0bf9fdb8f5680510041736666a9da8
Joe Khoury <joe.el.k97@gmail.com>
2016-06-13 01:51:04 -0400

    Wrote the tokenize function. It works!

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -10,24 +10,17 @@ Tokenizer::Tokenizer (const std::string& _file_name) : fr(_file_name) {
 }
 
 void Tokenizer::tokenize (char _delimiter) {
-    size_t start = 0;
-    size_t end   = (this->str_to_tokenize).find_first_of(_delimiter);
-
-    while (end <= std::string::npos) {
-        // Get the string from the start position to the delimiter.
-        std::string token_substr = (this->str_to_tokenize).substr(start, end-start);
-        if (token_substr.length() > 0) {
-            Token token_to_add(token_substr);
+    std::string token_str;
+    // Keep reading the file until the end. Never quit!
+    while (!(this->fr).done_reading) {
+        // Read the file until the delimiter.
+        token_str = (this->fr).get_string_up_to(_delimiter);
+        
+        if (token_str.length() > 0) {
+            Token token_to_add(token_str);
             // Remove any ugly characters from the token.
             token_to_add.cleanup_token();
             (this->tokens).push_back(token_to_add);
         }
-
-        if (end == std::string::npos) {
-            break;
-        }
-
-        start = end+1;
-        end = str_to_tokenize.find_first_of(_delimiter, start);
     }
 }
