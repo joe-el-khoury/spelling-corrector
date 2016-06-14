9c697f42370b43ce2763467e669eeceb702415b7
Joe Khoury <joe.el.k97@gmail.com>
2016-06-13 23:43:51 -0400

    Created the hashing fuction for the Token object.

--- src/TokenHistogram.cpp
+++ src/TokenHistogram.cpp
@@ -1,1 +1,8 @@
+#include <string>
+
 #include "TokenHistogram.h"
+#include "Token.h"
+
+TokenHistogram::TokenHistogram () {
+    this->histogram = TokenHistogram::Histogram();
+}
--- src/TokenHistogram.h
+++ src/TokenHistogram.h
@@ -1,7 +1,45 @@
 #ifndef TOKENHISTOGRAM_H
 #define TOKENHISTOGRAM_H
 
+#include <unordered_map>
+#include <string>
+
+#include "Token.h"
+
+/**
+ * Injecting a hash function into the standard namespace. Is it good practice
+ * to do this in the TokenHistogram object or the Token object? I think it should
+ * be better here because the Token object should't "know" that it is being hashed.
+ */
+namespace std {
+    /**
+     * Template specialization for hashing the Token object.
+     */
+    template<>
+    struct hash<Token> {
+        /**
+         * Overload the () operator to define a custom hash function.
+         */
+        size_t operator() (const Token& _other) const {
+            // Basically all I'm doing is hashing the string of the token object.
+            return std::hash<std::string>()(_other.get_token_str());
+        }
+    };
+}
+
 class TokenHistogram {
+    // The histogram is just an unordered map that maps strings to
+    // integers.
+    typedef std::unordered_map<Token, int> Histogram;
+public:
+    TokenHistogram ();
+private:
+    // The actual histogram son!
+    Histogram histogram;
+    
+    // The default value any token takes when added to the histogram or
+    // when an undefined token is accessed.
+    const int default_value = 1;
 };
 
 #endif /* TOKENHISTOGRAM_H */
