44ca4efc60ac94b882e243ca650121033fc2b941
Joe Khoury <joe.el.k97@gmail.com>
2016-06-14 21:36:59 -0400

    Set the done_reading flag to true if the file could not be opened properly.

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -13,7 +13,9 @@ FileReader::FileReader (const std::string& _file_name) {
     (this->fs).open(_file_name);
     if ((this->fs).fail()) {
         std::cout << "The file <" << _file_name << "> could not be opened." << std::endl;
-        (this->fail) = true;
+        this->fail = true;
+        // Hacky, but if opening the file fails then just assume reading is done.
+        this->done_reading = true;
     }
 }
 
