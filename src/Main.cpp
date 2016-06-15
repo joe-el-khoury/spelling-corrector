bd36653101beb609822927d0a3e21ba8e8a2be07
Joe Khoury <joe.el.k97@gmail.com>
2016-06-14 20:23:30 -0400

    Just to keep everything synced.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include "Tokenizer.h"
 
-/* DELETE THESE INCLUDE STATEMENTS LATER */
-#include <vector>
-#include <fstream>
-#include <algorithm>
-#include <memory>
+#include "Token.h"
+#include "TokenHistogram.h"
 
 int main (int argc, const char* argv[]) {
-	Tokenizer tk("Tes*ting tes/ting te#sting tes736ting");
-	unsigned int size = tk.tokens.size();
-	for (unsigned int i = 0; i < size; i++) {
-		std::cout << (tk.tokens)[i] << std::endl;
-	}
-	return 0;
+    TokenHistogram th;
+    th.add_token(Token("hello"));
+    th.add_token(Token("goodbye"));
+    th.add_token(Token("hello"));
+    th.add_token(Token("joe"));
+    th.print();
+    return 0;
 }
