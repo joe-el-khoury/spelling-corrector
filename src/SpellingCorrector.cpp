1b059ff754400c0d0c0ade7b6e75799a9a536c16
Joe Khoury <joe.el.k97@gmail.com>
2016-06-18 22:12:16 -0400

    Created the constructor.

--- src/SpellingCorrector.cpp
+++ src/SpellingCorrector.cpp
@@ -1,1 +1,11 @@
+#include <memory>
+
 #include "SpellingCorrector.h"
+#include "FileReader.h"
+#include "Tokenizer.h"
+#include "TokenHistogram.h"
+
+SpellingCorrector::SpellingCorrector () {
+    this->tokenizer = std::make_unique<Tokenizer>();
+    this->token_histogram = std::make_unique<TokenHistogram>();
+}
--- src/SpellingCorrector.h
+++ src/SpellingCorrector.h
@@ -1,10 +1,20 @@
 #ifndef SPELLINGCORRECTOR_H
 #define SPELLINGCORRECTOR_H
 
+#include <memory>
+
+#include "FileReader.h"
+#include "Tokenizer.h"
+#include "TokenHistogram.h"
+
 class SpellingCorrector {
 public:
-    SpellingCorrector();
-    ~SpellingCorrector();
+    SpellingCorrector ();
+private:
+    // The parts of the engine.
+    std::unique_ptr<FileReader> file_reader;
+    std::unique_ptr<Tokenizer> tokenizer;
+    std::unique_ptr<TokenHistogram> token_histogram;
 };
 
 #endif /* SPELLINGCORRECTOR_H */
