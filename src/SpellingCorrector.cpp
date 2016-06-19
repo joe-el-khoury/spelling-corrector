59e0eb25c2484d9650ca7394951e61cd9a82b96f
Joe Khoury <joe.el.k97@gmail.com>
2016-06-18 22:23:26 -0400

    Began work on a function that trains the spelling corrector.

--- src/SpellingCorrector.cpp
+++ src/SpellingCorrector.cpp
@@ -4,8 +4,15 @@
 #include "FileReader.h"
 #include "Tokenizer.h"
 #include "TokenHistogram.h"
+#include "Token.h"
 
 SpellingCorrector::SpellingCorrector () {
     this->tokenizer = std::make_unique<Tokenizer>();
     this->token_histogram = std::make_unique<TokenHistogram>();
+}
+
+/**
+ * Trains the spelling corrector on a file with the file name specified.
+ */
+void SpellingCorrector::train (const std::string& _file_name) {
 }
--- src/SpellingCorrector.h
+++ src/SpellingCorrector.h
@@ -1,6 +1,8 @@
 #ifndef SPELLINGCORRECTOR_H
 #define SPELLINGCORRECTOR_H
 
+#include <string>
+#include <vector>
 #include <memory>
 
 #include "FileReader.h"
@@ -10,11 +12,17 @@
 class SpellingCorrector {
 public:
     SpellingCorrector ();
+
+    // Trains the spelling corrector on a file.
+    void train (const std::string&);
 private:
     // The parts of the engine.
     std::unique_ptr<FileReader> file_reader;
     std::unique_ptr<Tokenizer> tokenizer;
     std::unique_ptr<TokenHistogram> token_histogram;
+    
+    // The names of the files we've already trained on.
+    std::vector<std::string> file_names;
 };
 
 #endif /* SPELLINGCORRECTOR_H */
