f5bbc823fec1e1f2a5659bacfdf4fadd021b5299
Joe Khoury <joe.el.k97@gmail.com>
2016-06-22 21:09:21 -0400

    Fixed previously unnoticed errors.

--- src/TokenEditor.cpp
+++ src/TokenEditor.cpp
@@ -1,24 +1,21 @@
-#include "TokenEditor.cpp"
+#include "TokenEditor.h"
 
 /**
  * Just like Python's awesome string splicing capabilities.
  * splice_token_from_to("hello", 0, 3) = "hel".
  */
-Token TokenEditor::splice_token_from_to (const Token& _to_splice, size_t _from, size_t _to) {
+Token splice_token_from_to (const Token& _to_splice, size_t _from, size_t _to) {
     std::string token_str = _to_splice.get_token_str();
-
-    std::string new_token_str = token_str.substr(_from, _to);
-    return Token(new_token_str);
+    return token_str.substr(_from, _to);
 }
 
 /**
  * Splits a token at the pivot.
  * split_at("joe", 1) = ("j", "oe").
  */
-TokenEditor::split_pair TokenEditor::split_at (const Token& _to_split, size_t _pivot) {
-    std::string token_str = _to_split.get_token_str();
-    Token first  = splice_token_from_to(token_str, 0, _pivot);
-    Token second = splice_token_from_to(token_str, _pivot, token_str.length());
+split_pair split_at (const Token& _to_split, size_t _pivot) {
+    Token first  = splice_token_from_to(_to_split, 0, _pivot);
+    Token second = splice_token_from_to(_to_split, _pivot, _to_split.get_token_str().length());
 
     return std::make_pair(first, second);
 }
@@ -27,14 +24,16 @@ TokenEditor::split_pair TokenEditor::split_at (const Token& _to_split, size_t _p
  * Creates edits of the token as splits.
  * get_split_edits("joe") = ("", "joe"), ("j", "oe"), ("jo", "e"), ("joe", "").
  */
-TokenEditor::splits get_split_edits (const Token& _to_edit) {
+splits TokenEditor::get_split_edits (const Token& _to_edit) {
+    std::string token_str = _to_edit.get_token_str();
+    
     // Construct the vector that will store the splits.
     // The vector's size will be the length of the string plus one.
     unsigned int str_length = _to_edit.get_token_str().length();
-    std::vector<TokenEditor::split_pair> ret(str_length+1);
+    splits ret(str_length+1);
 
     for (size_t i = 0; i <= (size_t)(str_length); ++i) {
-        ret[i] = split_at(_str, i);
+        ret[i] = split_at(token_str, i);
     }
 
     return ret;
--- src/TokenEditor.h
+++ src/TokenEditor.h
@@ -6,15 +6,13 @@
 
 #include "Token.h"
 
+// Custom types.
+typedef std::pair<Token, Token> split_pair;
+typedef std::vector<split_pair> splits;
+
 class TokenEditor {
-    // Custom types for splitting tokens.
-    typedef std::pair<Token, Token> split_pair;
-    typedef std::vector<split_pair> splits;
 public:
-    TokenEditor::splits get_split_edits (const Token&);
-private:
-    TokenEditor::split_pair split_at (const Token&, size_t);
-    Token splice_token_from_to (const Token&, size_t, size_t);
-}
+    splits get_split_edits (const Token&);
+};
 
 #endif /* TOKENEDITOR_H */
