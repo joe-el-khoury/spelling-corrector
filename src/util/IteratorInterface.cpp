200d0278f155aa413a22007520316d1c542e59f4
Joe Khoury <joe.el.k97@gmail.com>
2016-07-02 08:58:12 -0400

    Created the functions that allow you to add to the iterable.

--- src/util/IteratorInterface.cpp
+++ src/util/IteratorInterface.cpp
@@ -1,1 +1,22 @@
 #include "IteratorInterface.h"
+
+/**
+ * Add a vector to the already existing vector.
+ */
+template<typename T>
+void IteratorInterface<T>::add_to_iterable (const std::vector<T>& _to_add) {
+    // To prevent additional allocations.
+    (this->iterable).reserve((this->iterable).size() + _to_add.size());
+
+    std::move_iterator<T> move_iter_begin = std::make_move_iterator(_to_add.begin());
+    std::move_iterator<T> move_iter_end   = std::make_move_iterator(_to_add.end());
+    (this->iterable).insert((this->iterable).begin(), move_iter_begin, move_iter_end);
+}
+
+/**
+ * Add one element to the vector we will iterate on.
+ */
+template<typename T>
+void IteratorInterface<T>::add_to_iterable (const T& _to_add) {
+    (this->iterable).push_back(_to_add);
+}
--- src/util/IteratorInterface.h
+++ src/util/IteratorInterface.h
@@ -2,9 +2,17 @@
 #define ITERATORINTERFACE_H
 
 #include <iterator>
+#include <vector>
 
 template<typename T>
 class IteratorInterface : public std::iterator<std::input_iterator_tag, T> {
+public:
+    // Add to the list we will iterate on.
+    void add_to_iterable (const std::vector<T>&);
+    void add_to_iterable (const T&);
+private:
+    // The list we will iterate on.
+    std::vector<T> to_iterate;
 };
 
 #endif
