6a92855698915e39bd054ce54a1539e94f463af5
Joe Khoury <joe.el.k97@gmail.com>
2016-07-06 19:16:44 +0400

    Updates.

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,10 +8,20 @@ int main (int argc, const char* argv[]) {std::chrono::time_point<std::chrono::sy
 
     SpellingCorrector sp;
     std::cout << "+================Training================+" << std::endl;
-    start = std::chrono::system_clock::now();
-    sp.train("big.txt");
-    end = std::chrono::system_clock::now();
-    elapsed_seconds = end-start;
+    std::string to_train_on;
+    std::cout << "train (type \"done\" to finish)>> ";
+    while (std::getline(std::cin, to_train_on)) {
+        if (to_train_on == "done") {
+            break;
+        }
+        start = std::chrono::system_clock::now();
+        sp.train(to_train_on);
+        end = std::chrono::system_clock::now();
+        elapsed_seconds = end-start;
+        std::cout << "Training on <" << to_train_on << "> took " << elapsed_seconds.count();
+        std::cout << " second(s)." << std::endl;
+        std::cout << "train>> ";
+    }
     std::cout << "Training took " << elapsed_seconds.count() << " second(s)." << std::endl;
 
     std::cout << "+================Correcting================+" << std::endl;
