8bef185043087a52e65c8e707dc7260204b21483
Joe Khoury <joe.el.k97@gmail.com>
2016-07-13 14:59:58 +0400

    Added the things the trainer will use.

--- src/SpellingCorrectorTrainer.h
+++ src/SpellingCorrectorTrainer.h
@@ -1,7 +1,14 @@
 #ifndef SPELLINGCORRECTORTRAINER_H
 #define SPELLINGCORRECTORTRAINER_H
 
+#include <memory>
+
 class SpellingCorrectorTrainer {
+private:
+    // Used for training.
+    std::unique_ptr<FileReader> file_reader;
+    std::unique_ptr<Tokenizer> tokenizer;
+    std::unique_ptr<TokenHistogram> token_histogram;
 };
 
 #endif
