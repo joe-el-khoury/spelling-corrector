1c54edee4ad86358c9ca4e7dea5a7167a066f837
Joe Khoury <joe.el.k97@gmail.com>
2016-07-19 20:26:38 +0400

    Created a struct to hold information about a database.

--- src/util/MySQLInterface.cpp
+++ src/util/MySQLInterface.cpp
@@ -1,4 +1,8 @@
 #include "MySQLInterface.h"
 
-MySQLInterface::MySQLInterface () {
+MySQLInterface::MySQLInterface (const db_info& _db_info) {
+    // Get the information about the database.
+    std::string db_name     = _db_info.db_name;
+    std::string db_uname    = _db_info.db_uname;
+    std::string db_password = _db_info.db_password;
 }
--- src/util/MySQLInterface.h
+++ src/util/MySQLInterface.h
@@ -6,9 +6,18 @@
 #include <statement.h>
 #include <resultset.h>
 
+struct db_info {
+    std::string db_name;
+    std::string db_uname;
+    std::string db_password;
+
+    db_info (const std::string& _db_name, const std::string& _db_uname, const std::string& _db_password)
+        : db_name(_db_name), db_uname(_db_uname), db_password(_db_password) {}
+};
+
 class MySQLInterface {
 public:
-    MySQLInterface ();
+    MySQLInterface (const db_info&);
 };
 
 #endif
