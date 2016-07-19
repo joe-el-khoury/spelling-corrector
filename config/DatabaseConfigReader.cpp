98cdc4960b226908de3daa7c0b40f508d299d574
Joe Khoury <joe.el.k97@gmail.com>
2016-07-20 02:44:27 +0400

    Implemented the constructor.

--- config/DatabaseConfigReader.cpp
+++ config/DatabaseConfigReader.cpp
@@ -1,1 +1,11 @@
+#include <boost/property_tree/json_parser.hpp>
+
 #include "DatabaseConfigReader.h"
+
+/**
+ * All the constructor does is read the JSON file into the property tree.
+ * It's a small file and will not be in memory for long so it's alright.
+ */
+DatabaseConfigReader::DatabaseConfigReader (const std::string& _config_file_name) {
+    boost::property_tree::read_json(_config_file_name, this->json_tree);
+}
--- config/DatabaseConfigReader.h
+++ config/DatabaseConfigReader.h
@@ -2,9 +2,16 @@
 #define DATABASECONFIGREADER_H
 
 #include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
+
+#include <string>
+#include <unordered_map>
 
 class DatabaseConfigReader {
+public:
+    DatabaseConfigReader (const std::string&);
+    std::unordered_map<std::string, std::string> get_config_info ();
+private:
+    boost::property_tree::ptree json_tree;
 };
 
 #endif /* DATABASECONFIGREADER_H */
