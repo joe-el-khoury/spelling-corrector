88a06b06da028eecf23e7e1d83c93663aebac92e
Joe Khoury <joe.el.k97@gmail.com>
2016-07-19 20:55:48 +0400

    Added a new field to db_info.

--- src/util/MySQLInterface.cpp
+++ src/util/MySQLInterface.cpp
@@ -1,8 +1,14 @@
 #include "MySQLInterface.h"
 
 MySQLInterface::MySQLInterface (const db_info& _db_info) {
-    // Get the information about the database.
-    std::string db_name     = _db_info.db_name;
+    // Get the username and password of the database.
     std::string db_uname    = _db_info.db_uname;
     std::string db_password = _db_info.db_password;
+}
+
+std::string MySQLInterface::construct_url (const db_info& _db_info) {
+    std::string db_name       = _db_info.db_name;
+    std::string db_url_prefix = _db_info.db_url_prefix;
+    
+    return db_url_prefix + db_name;
 }
--- src/util/MySQLInterface.h
+++ src/util/MySQLInterface.h
@@ -7,17 +7,25 @@
 #include <resultset.h>
 
 struct db_info {
+    std::string db_url_prefix;
     std::string db_name;
     std::string db_uname;
     std::string db_password;
 
-    db_info (const std::string& _db_name, const std::string& _db_uname, const std::string& _db_password)
-        : db_name(_db_name), db_uname(_db_uname), db_password(_db_password) {}
+    db_info (const std::string& _db_url_prefix, const std::string& _db_name, const std::string& _db_uname, 
+            const std::string& _db_password)
+        : db_url_prefix(_db_url_prefix), db_name(_db_name), db_uname(_db_uname), db_password(_db_password) {}
 };
 
 class MySQLInterface {
 public:
     MySQLInterface (const db_info&);
+private:
+    std::string construct_db_url (const db_info&);
+
+    // The connection to the database.
+    sql::Connection* db_connection;
+    sql::ResultSet* last_result;
 };
 
 #endif
