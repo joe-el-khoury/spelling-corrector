afcbee6e5346566a094e1497a5f5ac711e791665
Joe Khoury <joe.el.k97@gmail.com>
2016-07-20 02:17:54 +0400

    Object to read config files.

---
+++ config/DatabaseConfigReader.h
@@ -0,0 +1,10 @@
+#ifndef DATABASECONFIGREADER_H
+#define DATABASECONFIGREADER_H
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+class DatabaseConfigReader {
+};
+
+#endif /* DATABASECONFIGREADER_H */
