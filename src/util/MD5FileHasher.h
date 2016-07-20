60badac2a26eacb966dac624a60da7c95c1106b5
Joe Khoury <joe.el.k97@gmail.com>
2016-07-20 19:24:01 +0400

    Implemented the hashing.

--- src/util/MD5FileHasher.h
+++ src/util/MD5FileHasher.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <string>
+#include <openssl/md5.h>
+#include <boost/iostreams/device/mapped_file.hpp>
 
 namespace MD5FileHasher {
 
@@ -10,7 +12,15 @@ namespace MD5FileHasher {
  * Get MD5 hash of a file.
  */
 std::vector<unsigned char> get_hash (const std::string& _file_name) {
-    return {};
+    unsigned char hash[MD5_DIGEST_LENGTH];
+
+    // Hash the file contents.
+    boost::iostreams::mapped_file_source src(_file_name);
+    MD5((unsigned char*)(src.data()), src.size(), hash);
+
+    // Convert the array to a vector and return it.
+    std::vector<unsigned char> ret(std::begin(hash), std::end(hash));
+    return ret;
 }
 
 } /* MD5FileHasher */
