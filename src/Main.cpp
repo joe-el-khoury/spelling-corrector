12a84ea3dcc0aebd8eeec3f8d3c67eb2bd086b22
Joe Khoury <joe.el.k97@gmail.com>
2016-07-24 01:33:24 +0400

    Train a file with the executable.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
-#include <chrono>
+#include <string>
 
 #include "SpellingCorrector.h"
+#include "SpellingCorrectorTrainer.h"
 
 int main (int argc, const char* argv[]) {
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    std::chrono::duration<double> elapsed_seconds;
-
-    SpellingCorrector sp;
-    std::cout << "+================Training================+" << std::endl;
-    std::string to_train_on;
-    std::cout << "train (type \"done\" to finish)>> ";
-    while (std::getline(std::cin, to_train_on)) {
-        if (to_train_on == "done") {
-            break;
-        }
-        start = std::chrono::system_clock::now();
-        sp.train(to_train_on);
-        end = std::chrono::system_clock::now();
-        elapsed_seconds = end-start;
-        std::cout << "Training on <" << to_train_on << "> took " << elapsed_seconds.count();
-        std::cout << " second(s)." << std::endl;
-        std::cout << "train>> ";
-    }
-    std::cout << "Training took " << elapsed_seconds.count() << " second(s)." << std::endl;
-
-    std::cout << "+================Correcting================+" << std::endl;
-    std::string to_correct;
-    Token corrected;
-    std::cout << "correct>> ";
-    while (std::getline(std::cin, to_correct)) {
-        start = std::chrono::system_clock::now();
-        corrected = sp.correct_word(Token(to_correct));
-        end = std::chrono::system_clock::now();
-        elapsed_seconds = end-start;
-        std::cout << "<" << to_correct << "> ===> <" << corrected.get_token_str() << "> ";
-        std::cout << "(" << elapsed_seconds.count() << " second(s)).\n" << std::endl;
-        std::cout << "correct>> ";
+    if (argc < 3) {
+        std::cout << "usage: SpellingCorrector --train <file>" << std::endl;
+        return 0;
+    } else if (std::string(argv[1]) != std::string("--train")) {
+        std::cout << "usage: SpellingCorrector --train <file>" << std::endl;
+        return 0;
+    } else {
+        SpellingCorrectorTrainer trainer;
+        std::string file_name(argv[2]);
+        std::cout << "Training..." << std::endl;
+        trainer.train(file_name);
     }
     return 0;
 }
