f99ba41e5fa12356943d14a02fc3f8c91889062f
Joe Khoury <joe.el.k97@gmail.com>
2016-07-26 02:38:32 +0400

    Start from scratch.

--- src/SpellingCorrector.h
+++ src/SpellingCorrector.h
@@ -11,25 +11,12 @@
 
 class SpellingCorrector {
 public:
-    SpellingCorrector ();
-    
-    // Used for training, and used for correcting.
-    void  train        (const std::string&, int=900);
-    Token correct_word (const Token&);
+    void correct_word (const Token&);
 private:
-    // The essential parts of the engine.
-    std::unique_ptr<FileReader> file_reader;
-    std::unique_ptr<Tokenizer> tokenizer;
-    std::unique_ptr<TokenHistogram> token_histogram;
-
     Token get_best_candidate (const std::vector<Token>&);
     std::vector<Token> get_known_edits_of (const Token&, unsigned int=1);
     void remove_unknown_words_from (std::vector<Token>&);
     bool is_known_word (const Token&) const;
-    
-    // The names of the files we've already trained on.
-    std::vector<std::string> files_trained_on;
-    bool already_trained_on (const std::string&) const;
 };
 
 #endif /* SPELLINGCORRECTOR_H */
