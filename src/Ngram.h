e040047e4e03c514dcc389767c791fc05e64c439
Joe Khoury <joe.el.k97@gmail.com>
2016-08-01 17:42:35 +0400

    Function prototypes.

--- src/Ngram.h
+++ src/Ngram.h
@@ -7,6 +7,16 @@
 #include "Token.h"
 
 class Ngram {
+public:
+    // Add a token to the list of tokens.
+    void add (const Token&);
+
+    // Whether there are anymore ngrams to get.
+    bool more (unsigned int);
+
+    std::vector<Token> get_ngram (unsigned int);
+private:
+    std::deque<Token> tokens;
 };
 
 #endif
