41afa15df2f3b6832cb871adde8c7a45e13c4a78
Joe Khoury <joe.el.k97@gmail.com>
2016-08-29 01:12:54 +0400

    Object to read JSON files.

---
+++ config/JSONReader.h
@@ -0,0 +1,11 @@
+#ifndef JSONREADER_H
+#define JSONREADER_H
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+class JSONReader {
+    JSONReader (const std::string&);
+};
+
+#endif /* JSONREADER_H */
