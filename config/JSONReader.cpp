fefa7f4f963aaca053313e6783952b027a23b551
Joe Khoury <joe.el.k97@gmail.com>
2016-08-29 01:42:31 +0400

    Put json data into a hash table.

--- config/JSONReader.cpp
+++ config/JSONReader.cpp
@@ -8,3 +8,19 @@ void JSONReader::read_file (const std::string& _file_name) {
         throw e;
     }
 }
+
+/**
+ * Loads the JSON data from the propery tree into a hash table.
+ */
+json_reader::json_data get_json_data () {
+    json_reader::json_data ret;
+    // Go through the JSON tree.
+    for (const bpt::ptree::value_type& row : this->json_tree) {
+        std::string key = row.first;
+        std::string val = row.second.data();
+
+        ret.insert({key, val});
+    }
+
+    return ret;
+}
