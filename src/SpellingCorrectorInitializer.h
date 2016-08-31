8ab411bb32ae548a42307334585b44721098b32a
Joe Khoury <joe.el.k97@gmail.com>
2016-08-31 16:59:45 +0400

    Function prototypes.

--- src/SpellingCorrectorInitializer.h
+++ src/SpellingCorrectorInitializer.h
@@ -1,7 +1,13 @@
 #ifndef SPELLINGCORRECTORINITIALIZER_H
 #define SPELLINGCORRECTORINITIALIZER_H
 
+#include "util/MySQLInterface.h"
+#include "../config/JSONReader.h"
+
 class SpellingCorrectorInitializer {
+public:
+    bool is_initialized ();
+    void initialize_spelling_corrector ();
 };
 
 #endif /* SPELLINGCORRECTORINITIALIZER_H */
