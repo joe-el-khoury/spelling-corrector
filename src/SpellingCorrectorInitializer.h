63944ba32b1f4a7b582817cfaf70f82da0e1166c
Joe Khoury <joe.el.k97@gmail.com>
2016-09-04 01:14:06 +0400

    Constructor & initializer, as well as flag to check if the initializer has been called before.

--- src/SpellingCorrectorInitializer.h
+++ src/SpellingCorrectorInitializer.h
@@ -1,6 +1,7 @@
 #ifndef SPELLINGCORRECTORINITIALIZER_H
 #define SPELLINGCORRECTORINITIALIZER_H
 
+#include <algorithm>
 #include <unordered_map>
 #include <vector>
 #include <memory>
@@ -11,9 +12,14 @@
 
 class SpellingCorrectorInitializer {
 public:
+    SpellingCorrectorInitializer ();
+    ~SpellingCorrectorInitializer ();
+    
     bool is_initialized ();
     void initialize ();
 private:
+    // Whether the user has checked if the spelling corrector has been initialized.
+    bool been_checked;
     std::vector<std::string> missing_databases;
     std::unordered_map<std::string, std::vector<std::string>> database_and_missing_tables;
 };
