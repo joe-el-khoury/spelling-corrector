c2466e35d01bee7b277de1f989eb230316107d3e
Joe Khoury <joe.el.k97@gmail.com>
2016-09-05 02:16:35 +0400

    Connect to the database.

--- src/SpellingCorrector.cpp
+++ src/SpellingCorrector.cpp
@@ -1,7 +1,16 @@
 #include <algorithm>
 
 #include "SpellingCorrector.h"
-#include "Token.h"
+
+SpellingCorrector::SpellingCorrector () {
+    // Throw an error if the spelling corrector is not initialized.
+    SpellingCorrectorInitializer spi;
+    if (!(spi.is_initialized())) {
+        throw std::runtime_error("Backend is not initialized.");
+    }
+
+    this->mysql_conn = std::make_unique<MySQLInterface>("training_data");
+}
 
 Token SpellingCorrector::correct_word (const Token& _to_correct) {
     // Silence, warnings!
--- src/SpellingCorrector.h
+++ src/SpellingCorrector.h
@@ -4,17 +4,26 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <exception>
+#include <stdexcept>
 
 #include "Token.h"
+#include "util/MySQLInterface.h"
+#include "SpellingCorrectorInitializer.h"
 
 class SpellingCorrector {
 public:
+    SpellingCorrector ();
+    
     Token correct_word (const Token&);
 private:
     Token get_best_candidate (const std::vector<Token>&);
     std::vector<Token> get_known_edits_of (const Token&, unsigned int=1);
     void remove_unknown_words_from (std::vector<Token>&);
     bool is_known_word (const Token&) const;
+
+    // Stuff related to the backend.
+    std::unique_ptr<MySQLInterface> mysql_conn;
 };
 
 #endif /* SPELLINGCORRECTOR_H */
